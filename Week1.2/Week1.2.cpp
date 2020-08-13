1ebddf050c5e198a84edd54b292efd8fd99a54ce
Phomphiriya <Phomphiriya@LAPTOP-36D1T06O>
2020-08-13 16:54:14 +0700

    Add project files.

---
+++ Week1.2/Week1.2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a = 20;
+	float x = 2.427;
+	float y = 3.362;
+	float z = x + y;
+	int t = a - z;
+
+	printf("%.2f\n", x + y);
+	printf("%d",t);
+	
+
+	return 0;
+}
